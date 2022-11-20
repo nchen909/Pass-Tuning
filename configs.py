18f768ad6ce7ecdc74652c09841ac35af65e00d2
1012598167 <chennuo909@163.com>
2022-11-20 15:15:57 +0000

    update configs

--- configs.py
+++ configs.py
@@ -168,12 +168,12 @@ def set_hyperparas(args):
 
     if args.few_shot == -1:
         if args.task in ['clone']:
-            args.num_train_epochs = 5 if not torch.cuda.is_available() else 10*torch.cuda.device_count()//2
+            args.num_train_epochs = 1 if not torch.cuda.is_available() else 2*torch.cuda.device_count()//2
             #for clone BCB full data!!!
             if args.is_classification_sample:
                 args.num_train_epochs = args.num_train_epochs * 10
         elif args.task in ['defect']:
-            args.num_train_epochs = 2*5 if not torch.cuda.is_available() else 4*5*torch.cuda.device_count()//2
+            args.num_train_epochs = 5 if not torch.cuda.is_available() else 10*torch.cuda.device_count()//2
             if args.is_classification_sample:
                 args.num_train_epochs = args.num_train_epochs * 10
         else:
