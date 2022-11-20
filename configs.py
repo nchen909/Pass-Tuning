2f228b81d442d96e956689b4e02fe451cf97b15a
1012598167 <chennuo909@163.com>
2022-11-20 15:05:23 +0000

    update configs

--- configs.py
+++ configs.py
@@ -168,16 +168,16 @@ def set_hyperparas(args):
 
     if args.few_shot == -1:
         if args.task in ['clone']:
-            args.num_train_epochs = 1 if not torch.cuda.is_available() else 2*torch.cuda.device_count()//2
+            args.num_train_epochs = 5 if not torch.cuda.is_available() else 10*torch.cuda.device_count()//2
             #for clone BCB full data!!!
             if args.is_classification_sample:
                 args.num_train_epochs = args.num_train_epochs * 10
         elif args.task in ['defect']:
-            args.num_train_epochs = 2 if not torch.cuda.is_available() else 4*torch.cuda.device_count()//2
+            args.num_train_epochs = 2*5 if not torch.cuda.is_available() else 4*5*torch.cuda.device_count()//2
             if args.is_classification_sample:
                 args.num_train_epochs = args.num_train_epochs * 10
         else:
-            args.num_train_epochs = 50 if not torch.cuda.is_available() else 50*torch.cuda.device_count()//2
+            args.num_train_epochs = 60 if not torch.cuda.is_available() else 60*torch.cuda.device_count()//2
 
         if args.model_name in ['t5', 'codet5']:
             args.batch_size = 16  if not torch.cuda.is_available() else 16 * torch.cuda.device_count()
@@ -202,4 +202,4 @@ def set_hyperparas(args):
     elif args.few_shot < 2048: #512,1024
         args.num_train_epochs = 32
         args.batch_size = 8 if args.model_name not in ['t5', 'codet5'] else 4
-    args.patience = min( 10, args.num_train_epochs//5)
+    args.patience = min( 10, args.num_train_epochs//5*5)
